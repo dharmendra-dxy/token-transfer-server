9e2e7fbffdddfad106e977c21f73d41b0227b199
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2025-01-26 12:21:26 +0530

    added route: account/transfer

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -1,4 +1,6 @@
+const {mongoose } = require("mongoose");
 const Account = require("../models/account.model");
+
 
 //[GET]  handleGetBalance:
 async function handleGetBalance(req,res){
@@ -22,10 +24,45 @@ async function handleGetBalance(req,res){
 
 //[POST]  handlePostTransferMoney:
 async function handlePostTransferMoney(req,res){
-    
-    const userid = req.userid;
-    
 
+    //  start session:
+    const session = await mongoose.startSession(); 
+
+    // start transaction:
+    session.startTransaction();
+    const {amount, receiverId} = req.body();
+    
+    const senderAccount = await Account.findOne({userId: req.userid}).session(session);
+
+    if(!senderAccount || senderAccount.balance < amount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Insufficient account balance",
+            success: false,
+        });
+    }
+
+    const receiverAccount = await Account.findOne({userId: receiverId}).session(session);
+
+    if(!receiverAccount){
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "No such receiver account exsits",
+            success: false,
+        });
+    }
+
+    // perform transactions:
+    await Account.updateOne({userid: req.userid}, {$inc: {balance: -amount}}).session(session);
+    await Account.updateOne({userid: receiverId}, {$inc: {balance: amount}}).session(session);
+
+    // commit the transaction:
+    await session.commitTransaction();
+
+    return res.status(200).json({
+        message: "Transfer made succesfully",
+        success: true,
+    });
 
 }
 
